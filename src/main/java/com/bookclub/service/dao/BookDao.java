a21c7b6356391948d6940daac6ce00b22b954185
Syadlapalli <Syadlapalli@my365.bellevue.edu>
2023-09-15 11:54:10 +0530

    Week3 assignment

---
+++ src/main/java/com/bookclub/service/dao/BookDao.java
@@ -0,0 +1,7 @@
+package com.bookclub.service.dao;
+
+import com.bokclub.service.GenericDao;
+import com.bookclub.model.Book;
+
+public interface BookDao extends GenericDao<Book, String>{
+}
--- src/main/java/com/bookclub/web/HomeController.java
+++ src/main/java/com/bookclub/web/HomeController.java
@@ -1,10 +1,17 @@
 package com.bookclub.web;
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bookclub.model.Book;
+import com.bookclub.service.dao.BookDao;
+import com.bookclub.service.impl.MemBookDao;
 
 /**
  *
@@ -21,9 +28,28 @@ public class HomeController {
 
 
   @RequestMapping(method = RequestMethod.GET)
-  public String showHome(Model model) {
+ public String showHome(Model model) {
+	    BookDao booksDao = new MemBookDao();
+	    List<Book> books = booksDao.list();
+	    for (Book book : books){
+
+	    System.out.println(book.toString());
+	    }
+	    model.addAttribute("books", books);
 	    return "index";
-  }
+ }
+  @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+ public String getMonthlyBook(@PathVariable("id") String id, Model model) {
+	    String isbn=id;
+	    System.out.println(id);
+	    MemBookDao booksDao = new MemBookDao();
+	    Book book = booksDao.find(isbn);
+	    System.out.println(book.toString());
+
+	    model.addAttribute("book", book);
+
+	    return "monthly-books/view";
+ }
    @RequestMapping(method = RequestMethod.GET, path = "/about")
   public String showAboutUs() {
     return "about";
